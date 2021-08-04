b49aa0ad47c2d0f6fadc56f6a1f24af7ea69d1a1
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-04 14:28:48 -0300

    Um pouco mais sobre módulos

--- calc.js
+++ calc.js
@@ -1,19 +1,7 @@
-let calc = {
-  mult: (x, y) => {
-    return x * y
-  },
-
-  soma: (x, y) => {
-    return x + y
-  }
+module.exports.soma = function soma(x, y) {
+  return x + y
 }
 
-module.exports = calc
-
-// module.exports = function mult(x, y) {
-//   return x * y
-// }
-
-// module.exports = function soma(x, y) {
-//   return x + y
-// }
+module.exports.mult = function mult(x, y) {
+  return x * y
+}
--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-calc = require('./calc')
+// let calc = require('./calc')
 
-let args = process.argv.slice(2)
+let mult = require('./calc').mult
 
-// console.log(args)
-
-let a = Number(args[1])
-let b = Number(args[2])
-let c = ''
-
-if (args[0] == 's') {
-  c = calc.soma(a, b)
-} else if (args[0] == 'm') {
-  c = calc.mult(a, b)
-} else {
-  c = 'Opção inválida'
-}
-
-console.log(c)
+console.log(mult(3, 4))
