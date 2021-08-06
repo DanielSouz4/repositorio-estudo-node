abb301a59696c8dccd5e2b1d7096cbb09fd09e2e
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-06 15:30:40 -0300

    Request*

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 
+function handleFile(req, res, callBack) {
+  let path = url.parse(req.url).pathname
+
+  let fileName = '.' + path
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      if (callBack) {
+        if (!callBack(req, res)) {
+          res.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' })
+          res.end('<h1>Página não encontrada</h1>')
+        }
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.write(data)
+      res.end()
+    }
+  })
+}
+
+function handleRequest(req, res) {
+  let path = url.parse(req.url).pathname
+
+  if (path == '/Teste') {
+    res.end('Teste')
+    return true
+  }
+
+  return false
+}
+
 http
   .createServer((request, response) => {
-    let path = url.parse(request.url).pathname
-
-    if (path == '' || path == '/') {
-      path = '/index.html'
-    }
-
-    let fileName = '.' + path
-
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        response.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' })
-        response.end('<h1>Página não encontrada</h1>')
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write(data)
-        response.end()
-      }
-    })
+    handleFile(request, response, handleRequest)
   })
   .listen(3000, err => {
     if (err) {
