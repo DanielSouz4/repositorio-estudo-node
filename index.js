b6fd9bee8f8de836446114c9788b1b7b42ee99f6
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-20 16:43:55 -0300

    Arquivos estáticos com Express

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const path = require('path')
+// ajudar a montar caminho dos arq
 
-// app recebe objeto express (criar esse objeto express rodando o construtor)
 const app = express()
+// app recebe objeto express (criar esse objeto express rodando o construtor)
 
-// fazer app excutar requisiçõeas http
+app.use('/meusite', express.static(path.join(__dirname, 'client')))
+// '/meusite' -> arq apenas servidos quando estiver um /meusite depois do endereço
+// app.use(express.static()) -> p/ servir arq estáticos (primeir. procura o index.html)
+// path.join(__dirname, 'cliente') -> passar o caminho
+// __dirname - > colocado no lugar do "."
+// 'client' - > pasta onde está os arq estáticos
+
 const PORT = 5000
 app.listen(PORT, () => {
   console.log(`Server Running on Port:${PORT}`)
 })
+// fazer app excutar requisiçõeas http
