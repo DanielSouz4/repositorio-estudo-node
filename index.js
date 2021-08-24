60dda6c6e49e0f680a5acdf997b9c0e10e396851
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-24 13:54:32 -0300

    Rotas

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 var express = require('express')
 var app = express()
-var bodyPaser = require('body-parser')
 
-let consoleBody = (req, res, next) => {
-  console.log(req.body)
-  next()
-}
+let alunos = [
+  { id: 0, nome: 'jose' },
+  { id: 1, nome: 'Maria' },
+  { id: 2, nome: 'Joao' },
+  { id: 3, nome: 'Marcos' }
+]
 
-let hello = (req, res) => {
+app.use(express.urlencoded())
+// pega os dados da requisição no formato form
+
+app.get('/', (req, res) => {
   res.send('hello World')
-}
+})
 
-app.use('/', bodyPaser.json())
-app.use('/', consoleBody)
-app.get('/', hello)
+app.get('/alunos', (req, res) => {
+  res.json(JSON.stringify(alunos))
+})
 
-app.post('/', hello)
+// sistema que recebe um id através do formulário e retorna um aluno correspondente
+app.get('/aluno', (req, res) => {
+  console.log(req.body)
+  console.log(req.query)
+  let aluno = alunos[req.query.id]
+  res.json(aluno)
+})
+
+// outra maneira (permite chamadas ex: localhost:300/aluno/2)
+// app.get('/aluno/:id', (req, res) => {
+//   console.log(req.params.id)
+//   let aluno = alunos[req.params.id]
+//   res.json(aluno)
+// })
+
+// outra maneira usando query
+// rota?id=2
 
 app.listen(3000, () => console.log('Server rodando...'))
 
-// uso prático body parser mdd muito utilizado
-// (imprime corpo da require)
-// npm install body-parser
+// rota é o path ex: "/alunos"
