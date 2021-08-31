0af3a19f61c83624f0fa4f896732067c17da3478
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-31 16:41:58 -0300

    Substituição de variáveis

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ var fs = require('fs')
 const path = require('path')
 var app = express()
 
+let user = {
+  name: 'Daniel',
+  phone: '(111)125411'
+}
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('user')
+  // res.render('user', { name: 'Daniel Souza', phone: '(234)54321' })
+  res.render('user', user)
 })
 
 app.listen(3000, () => console.log('Server Running on 3000'))
 
-// configurar:
-// npm install ejs
-// 1 - set qual pasta irão estar os templates (usar path pra ajudar com o caminho)
-// 2 - set qual template que está usando
-
-// usar
-// criar arquivo .ejs na pasta wiews
-// para servir usar res.render('user')
-// render() => p/ renderizar uma view através do template engine
+// p/ preparar o template para um valor que seja variável
+// usar padrão do ejs <%=name%> no template ejs
+// se tirar o (=) não aparece a variável (o = indica que tem uma saída)
+// no node passar os valores que quer substituir no formato de obj
