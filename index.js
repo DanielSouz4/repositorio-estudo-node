62d110d1f0e0372867208bae59fd89001065ef9f
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-08-31 17:24:24 -0300

    For no EJS

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ var fs = require('fs')
 const path = require('path')
 var app = express()
 
-let user = {
-  name: 'Daniel',
-  phone: '(111)125411'
-}
+let users = [
+  {
+    id: 0,
+    name: 'Daniel',
+    phone: '(111)125411'
+  },
+  {
+    id: 1,
+    name: 'José',
+    phone: '(111)125411'
+  },
+  {
+    id: 2,
+    name: 'Marcos',
+    phone: '(111)125411'
+  }
+]
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  // res.render('user', { name: 'Daniel Souza', phone: '(234)54321' })
-  res.render('user', user)
+  res.render('user', { users })
 })
 
 app.listen(3000, () => console.log('Server Running on 3000'))
 
-// p/ preparar o template para um valor que seja variável
-// usar padrão do ejs <%=name%> no template ejs
-// se tirar o (=) não aparece a variável (o = indica que tem uma saída)
-// no node passar os valores que quer substituir no formato de obj
+// como fazer loop no template engine
+// usar for dentro do html
+
+//(nao coloca o = no for se não quiser apareern na tela)
+//
