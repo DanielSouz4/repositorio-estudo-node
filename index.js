511796b4bed01a61fa8230d84df1cc230a91c505
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-09-01 11:01:16 -0300

    Partials

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ app.get('/', (req, res) => {
   res.render('user', { users })
 })
 
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
 app.listen(3000, () => console.log('Server Running on 3000'))
 
-// como fazer if no template engine
+// partials => para fazer um elemento que vai está em várias páginas
+// crair pasta partials
+// crair arq .ejs para esse elemento específico
+
+// para incluir esse elemento em alguma pág
+// <%- include("./partials/navbar.ejs")%>
+// <%- include("partials/navbar")%>
+// não precisa do ./ nem .ejs já esta set em app.set('views', path.join(__dirname, 'views'))
+//
+// pode para passar uma variável no partials
+// <h1>Logo - <%=title%></h1>
+// e passar um valor pra essa variável quando for incluir o partials em páginas
+//<%- include("partials/navbar", {title: "About"})%>
